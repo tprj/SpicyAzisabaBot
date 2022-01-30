7f2c4d274d61479ed54f5323cc40cdb31e4cf3b0
acrylic-style <me@acrylicstyle.xyz>
2022-01-31 02:24:35 +0900

    Fix bots were able to create threads

--- src/main/kotlin/net/azisaba/spicyAzisabaBot/messages/RealProblemChannelHandler.kt
+++ src/main/kotlin/net/azisaba/spicyAzisabaBot/messages/RealProblemChannelHandler.kt
@@ -8,6 +8,7 @@ object RealProblemChannelHandler : MessageHandler {
     override fun canProcess(message: Message): Boolean = message.channelId == Snowflake(760438206847123497L)
 
     override suspend fun handle(message: Message) {
+        if (message.author?.isBot != false) return
         val content = message.content.lines().getOrNull(0) ?: return
         (message.channel.fetchChannel() as TextChannel).startPublicThread(content) {
             this.invitable = true
